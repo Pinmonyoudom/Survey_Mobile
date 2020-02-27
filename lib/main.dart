2c40f4d3480f02e7f220c1a09f51c015af35c2b3
pinmonyoudom <pinmonyoudom718@gmail.com>
2020-02-28 00:12:02 +0700

    divide to login and register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:login/register.dart';
+import 'login.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: MyApp(),
+    home: HomePage(),
   ));
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<MyApp> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter'),
+    return MaterialApp(
+      title: 'Survey',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Login Page',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            FlatButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              textColor: Colors.blue,
-              child: Text('Forgot Password'),
-            ),
-            Container(
-                height: 50,
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: RaisedButton(
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  child: Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )),
-            Container(
-                child: Row(
-              children: <Widget>[
-                Text('Does not have account?'),
-                FlatButton(
-                  textColor: Colors.blue,
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (ctx) {
-                        RegisterPage();
-                      }),
-                    );
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ))
-          ],
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
