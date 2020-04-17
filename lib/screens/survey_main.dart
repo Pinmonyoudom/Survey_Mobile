58a9e256f8b4034ae4d4e73adf1ea3ff70b92110
SopheaPrak <sopheaprak2@gmail.com>
2020-04-17 13:29:44 +0700

    Update logout

--- lib/screens/survey_main.dart
+++ lib/screens/survey_main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:login/screens/login.dart';
+import 'package:login/utils/api.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Survey extends StatefulWidget {
@@ -10,41 +12,48 @@ class Survey extends StatefulWidget {
 class _SurveyState extends State<Survey>{
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.only(top: 150),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: RaisedButton(
-                      elevation: 10,
-                      onPressed: (){
-                        logout();
-                      },
-                      color: Color(0xff01A0C7),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
-                      child: Text('Logout',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15.0,
-                          decoration: TextDecoration.none,
-                          fontWeight: FontWeight.normal,
-                        ),
+      return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 150),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: RaisedButton(
+                    elevation: 10,
+                    onPressed: (){
+                      logout();
+                    },
+                    color: Color(0xff01A0C7),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Text('Logout',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15.0,
+                        decoration: TextDecoration.none,
+                        fontWeight: FontWeight.normal,
                       ),
                     ),
                   ),
-                ],
-              ),
-          ),
-        );
-      }
+                ),
+              ],
+            ),
+        ),
+      );
+    }
 
   void logout() async{
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    localStorage.remove('user');
-    localStorage.remove('token');
-    Navigator.push(context,
-      MaterialPageRoute(builder: (context)=>LoginPage()));
+    var res = await Network().getData('/logout');
+    var body = json.decode(res.body);
+    if(body['success']){
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      localStorage.remove('user');
+      localStorage.remove('token');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context)=>LoginPage()
+        )
+      );
+    }
   }
 }
